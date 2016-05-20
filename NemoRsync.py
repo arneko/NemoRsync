bdef00d5232fc98bd756089461415965564aca5c
Arne Ko <schoenheit76@gmx.li>
2016-05-20 18:30:57 +0200

    Code cleanup

--- NemoRsync.py
+++ NemoRsync.py
@@ -6,12 +6,13 @@ from urlparse import urlparse
 import urllib2
 from gi.repository import Gtk, Gdk
 
+#Need --protect-args so I don't need to do any further magic with the paths (e.g. " "->"\\\ "), using --inplace should be optional
 rsync_args="-aP --protect-args --inplace"
 #Will try integrating zenity later
 use_Zenity=False 
 
 def rsyncify(uri):
-    """Produces a rsync compatible path"""
+    """Produces an rsync compatible path"""
     data = urlparse(uri)
     path = urllib2.unquote(data.path)
     if(data.scheme=="sftp"):
@@ -26,23 +27,21 @@ def rsyncify(uri):
         rsync_url="\"%s\""%(path)    
     return rsync_url
 
-
-target_uri=sys.argv[1]
-
 #Get clipboard if it's copied files
 clipboard = Gtk.Clipboard.get(Gdk.SELECTION_CLIPBOARD)
 result = clipboard.wait_for_contents(Gdk.Atom.intern("x-special/gnome-copied-files", False))
 str_clipboard=result.get_data()
 
+dest_uri=sys.argv[1]
+dest_rsync_path=rsyncify(target_uri)
+
 #rsync file by file
 for clipboard_uri in str_clipboard.splitlines()[1:]:
-    rsync_url1=rsyncify(clipboard_uri)
-    rsync_url2=rsyncify(target_uri)
+    src_rsync_path=rsyncify(clipboard_uri)
     if use_Zenity:
         #This was taken from http://www.davidverhasselt.com/zenity-rsync-and-awk/ and would probably work for folders.
-        cmd="rsync %s %s %s |awk -f rsync.awk |zenity --progress --title \"Backing up USB-Stick\" --text=\"Scanning...\" --percentage=0 --auto-kill"%(rsync_args,rsync_url1,rsync_url2)
+        cmd="rsync %s %s %s |awk -f rsync.awk |zenity --progress --title \"Backing up USB-Stick\" --text=\"Scanning...\" --percentage=0 --auto-kill"%(rsync_args,src_rsync_path,dest_rsync)
     else:
-        cmd="rsync %s %s %s"%(rsync_args,rsync_url1,rsync_url2)   
-    print type(result)
+        cmd="rsync %s %s %s"%(rsync_args,src_rsync_path,dest_rsync)
+    #Execute command
     os.system(cmd)
-
